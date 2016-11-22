bea4928919ad57a0091aa5bb3201d6c93e37620f
Ashvin Rai <ashvinrai@gmail.com>
2016-11-21 23:46:34 -0500

    sliding window

--- FTA-client.py
+++ FTA-client.py
@@ -22,7 +22,7 @@ SOURCE_PORT = int(sys.argv[2])
 window = 1
 State = "NONE"
 seq = 0
-acknum = 0
+acknum = 1
 dest_addr = (SOURCE_IP, int(SOURCE_PORT))
 while (True):
    word = str(raw_input('Commands: connect, get <filename>, window <size>: '))
@@ -45,15 +45,35 @@ while (True):
           State = "CONNECTED"
    if (word[0] == "get"):
       if State == "CONNECTED":
-        acknum = 1
+        acknum = 0
         filename = word[1]
         log('get command with filename: ' + filename)
         packet = util.make_packet("", SOURCE_PORT, seq, acknum, True, False, False, window, "get " + filename)
         s.sendto(packet, dest_addr)
         ##TO-DO
         ##Sent filename to server. Server must now send the file back
-        msg, addr = s.recvfrom(4096)
-        print msg
+        packets = []
+        end = False
+        while end != True:
+          x = 0
+          while x < window:
+            packet, addr = s.recvfrom(4096)
+            print packet
+            header, data, checksum = util.unpack_packet(packet)
+            if str(acknum) == header[2] and header[4]:
+              packets.append(packet)
+              x += 1
+              acknum += 1 
+              if header[6] == "True":
+                packet = util.make_packet("", SOURCE_PORT, seq, acknum, False, True, True, window, "END")
+                s.sendto(packet, dest_addr)
+                x = window
+                end = True
+            else:
+              x = window
+              print "missing or damaged packet"
+          packet = util.make_packet("", SOURCE_PORT, seq, acknum, False, True, False, window, "ACK")
+          s.sendto(packet, dest_addr)
       else:
         print 'Cannot get a file: You did not create a connection yet!'
    if (word[0] == "window"):
--- FTA-server.py
+++ FTA-server.py
@@ -18,7 +18,7 @@ elif len(sys.argv) > 2:
         print 'Too many arguments! Must input FTA-Server.py and port (optional: \'-d\' for debug mode)'
         sys.exit()
 
-window = 5
+window = 1
 window_pointer = 0
 SOURCE_PORT = int(sys.argv[1])
 seq = 0
@@ -44,8 +44,6 @@ while (not_connected):
 
 while (True):
 	packet, addr = s.recvfrom(4096)
-	print packet
-	print addr
 	header, data, checksum = util.unpack_packet(packet)
 	cmd = data.split(" ")[0]
 	print 'cmd =', cmd
@@ -59,13 +57,36 @@ while (True):
 		packets = []
 		x = 0
 		while x < len(data):
-			seq += 1
-			packet = util.make_packet("", SOURCE_PORT, seq, acknum, False, False, False, window, data[window_pointer])
+			if x == len(data) - 1:
+				packet = util.make_packet("", SOURCE_PORT, x, acknum, True, False, True, window, data[x])
+			else:
+				packet = util.make_packet("", SOURCE_PORT, x, acknum, True, False, False, window, data[x])
 			packets.append(packet)
-			window_pointer += 1
 			x += 1
-		print packets[0]
-		s.sendto(packets[0], addr)
+		
+		#first [window] packets sent
+		while window_pointer < window:
+			s.sendto(packets[window_pointer], addr)
+			window_pointer+=1
+
+		packet, addr = s.recvfrom(4096)
+		print packet
+		header, data, checksum = util.unpack_packet(packet)
+		seq = int(header[3])
+		end = "False"
+		while end != "True": 
+			window_pointer = 0
+			while window_pointer < window:
+				s.sendto(packets[window_pointer + seq], addr)
+				if(seq+window_pointer == len(packets) - 1):
+					window_pointer = window
+				else:
+					window_pointer+=1
+			packet, addr = s.recvfrom(4096)
+			print packet
+			header, data, checksum = util.unpack_packet(packet)
+			seq = int(header[3])
+			end = header[6]
 
 	##This method of getting user input cannot work; can have a diff thread for listening to user input instead
 	#word = str(raw_input(''))
