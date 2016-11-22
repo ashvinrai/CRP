547bbfffdf3c014596238a24ffd841bc1bd71768
vkondeti <vkondeti@gatech.edu>
2016-11-22 00:41:10 -0500

    fixed checksum

--- util.py
+++ util.py
@@ -4,7 +4,8 @@ import array
 def make_packet(source_port, dest_port, seq, acknum, syn, ack, end, window, data):
 	#flags = 0 | (SYN << 2) | (ACK << 1) | END 
 	header = "%s|%i|%i|%i|%r|%r|%r|%i" % (str(source_port), int(dest_port), int(seq), int(acknum), syn, ack, end, int(window)) 
-	checksum = create_checksum(header + data)
+	headerPart = str(header.split('|'))
+	checksum = create_checksum(headerPart + data)
 	packet = "%s|%s|%s" % (header, data, checksum)
 	#SYN, ACK, END = 0, 1, 1
 	#00000000 | (SYN << 2) | (ACK << 1) | END 
@@ -22,7 +23,7 @@ def create_checksum(packet):
 
 def check_checksum(packet):
 	header, data, checksum = unpack_packet(packet)
-	return create_checksum(header + data) == checksum
+	return create_checksum(str(header) + str(data)) == str(checksum)
 
 def request_file(filename):
 
